04532700aadaa0e2d95f9c17bd2f3b5d96026a68
Vladimir Makarić <vladamakaric@gmail.com>
2015-03-12 06:38:20 +0100

    add nav buttons

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 $("#main-content").load("home.html");
 
+var list = $("#theoryToggle li");
+var listCount = list.length;
+var theoryPageCallbacks = [];
 
-$("#home").click(function(){
-	 $("#main-content").load("home.html");
-});
+$.each(list, function (index, data) {
 
-$("#theory1").click(function(){
-	 $("#main-content").load("theory1.html");
-});
+	theoryPageCallbacks[index] = function(){
 
-$("#theory2").click(function(){
-	 $("#main-content").load("theory2.html");
-});
+		var liid = $(list[index]).attr("id");
 
-$("#theory3").click(function(){
-	 $("#main-content").load("theory3.html", function() {
-		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-	 });
+		 $("#main-content").load(liid + ".html", function() {
+			MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+
+			$(this).append("<hr>");
+
+			var pager = $("<ul></ul>"); 
+			pager.attr("class","pager");
+
+		
+			if(index!=listCount-1){
+				var btn = $('<li>', {class: 'next'});
+				btn.click(theoryPageCallbacks[index+1]);
+				btn.append($('<a>', {href: '#', text: 'Next'}));
+				pager.append(btn);
+
+			}
+
+			if(index>0){
+				var btn = $('<li>', {class: 'previous'});
+				btn.click(theoryPageCallbacks[index-1]);
+				btn.append($('<a>', {href: '#', text: 'Previous'}));
+				pager.append(btn);
+			}
+
+			$(this).append(pager);
+
+		 });
+	}
+
+    $(this).click(theoryPageCallbacks[index]);
 });
